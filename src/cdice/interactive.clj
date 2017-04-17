b0eb7e93f8d4729c5d4040f121d7528832895c93
Yovan Naumovski <jovan3.14@gmail.com>
2017-04-17 15:20:32 +0300

    set remaining player dice state on initialization.

--- src/cdice/interactive.clj
+++ src/cdice/interactive.clj
@@ -4,7 +4,7 @@
             [cdice.turn :as turn]))
 
 (def last-play (atom nil))
-(def players_set (atom nil))
+(def players_dice_remain (atom {}))
 
 (defn enter-player-names []
   (println "How many players: ")
@@ -15,4 +15,15 @@
 
 (defn init-players [players_list]
   {:pre [(and (some? players_list) (not (empty? players_list)))]}
-  (map (fn [player] (list player (roll/roll 5))) players_list))
+  (doseq [player players_list]
+    (swap! players_dice_remain assoc-in [(keyword player)] 5))
+  (map #(list % (roll/roll 5)) players_list))
+
+(defn make-last-play [player_name value how_many]
+  {:player player_name :guess {:value value :how_many how_many}})
+
+(defn do-turn [players_set value how_many]
+  (let [player (first players_set)
+        player_name (first player)]
+    (reset! last-play (make-last-play player_name value how_many))))
+
--- test/cdice/interactive_test.clj
+++ test/cdice/interactive_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [cdice.interactive :as ia]))
 
+(defn init-players-fixture [f]
+  (reset! ia/players_dice_remain {})
+  (f))
+
+(use-fixtures :each init-players-fixture)
+
 (deftest test-init-players
   (testing "should throw exception if nil or empty list of player provided"
     (is (thrown? java.lang.AssertionError (ia/init-players nil)))
@@ -11,6 +17,19 @@
     (is (every? #(first %) (ia/init-players '("player1" "player2"))))
     (is (every? #(= 5 (count (second %))) (ia/init-players '("pl1" "pl2" "pl3"))))))
 
+(deftest test-init-players-state
+  (testing "should set the players_dice_remain atom"
+    (ia/init-players '("pl1" "pl2" "pl3"))
+    (is (= (:pl1 @ia/players_dice_remain) 5))
+    (is (= (:pl2 @ia/players_dice_remain) 5))
+    (is (= (:pl3 @ia/players_dice_remain) 5))
+    (is (= (:pl4 @ia/players_dice_remain) nil))))
+
+(def players_set '(("player1" '(2 2 2 2 2)) ("player2" '(3 3 3 3 3))))
+
 (deftest test-do-turn
   (testing "should change the last play reference"
-    (is (= nil @ia/last-play))))
+    (reset! ia/last-play nil)
+    (ia/do-turn players_set 2 6)
+    (is (= {:player "player1" :guess {:value 2 :how_many 6}} @ia/last-play))))
+
